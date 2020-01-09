82d1bb1b1de9ba1105333ca9255ce8f9136b8cbe
perrywzm <pwangzhi@cn.eit>
2020-01-09 11:25:57 +0800

    Updates

---
+++ precache-manifest.1dc62767e55e9734f9928d20129e5da1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f07efc5d359a2cc6e90c208bdc5fbb4",
+    "url": "/CNY_Frontend/index.html"
+  },
+  {
+    "revision": "71e4187dc3eeaefa9124",
+    "url": "/CNY_Frontend/static/css/main.4e9e7bfa.chunk.css"
+  },
+  {
+    "revision": "986174506d26881ea703",
+    "url": "/CNY_Frontend/static/js/2.a6313793.chunk.js"
+  },
+  {
+    "revision": "749849a1b8b3daee3cba1ec55bc8cddd",
+    "url": "/CNY_Frontend/static/js/2.a6313793.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71e4187dc3eeaefa9124",
+    "url": "/CNY_Frontend/static/js/main.6c1bdff4.chunk.js"
+  },
+  {
+    "revision": "c22627f211a9e620c037",
+    "url": "/CNY_Frontend/static/js/runtime-main.1a52a2e4.js"
+  },
+  {
+    "revision": "5bcd2e82a612503de0e18d684011a612",
+    "url": "/CNY_Frontend/static/media/SentyWEN2017.5bcd2e82.ttf"
+  },
+  {
+    "revision": "e2b23dc7baefa6ab02b49bd50a6f9f71",
+    "url": "/CNY_Frontend/static/media/bg.e2b23dc7.png"
+  },
+  {
+    "revision": "701fe22b33b1a1b747f987953c1a67b6",
+    "url": "/CNY_Frontend/static/media/cherryblossom.701fe22b.png"
+  },
+  {
+    "revision": "4ca3826ed4cee7655cf57a7a85ef42cd",
+    "url": "/CNY_Frontend/static/media/cntxt.4ca3826e.svg"
+  }
+]);
