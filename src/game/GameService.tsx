83c5d1cceab2a01019f43e70737f15b4237acc5c
perrywzm <pwangzhi@cn.eit>
2020-01-09 10:30:28 +0800

    Added event handler for GameState socket event

--- src/game/GameService.tsx
+++ src/game/GameService.tsx
@@ -1,29 +1,49 @@
 import AjaxService from "../services/AjaxService";
 import { BaseDependency } from "../services/DependencyInjector";
 import Question from "../models/Question";
+import { GameState, ProgressState, QuestionState } from "../models/GameState";
 
 export default class GameService extends BaseDependency {
+  gameState: ProgressState.EMPTY;
+  questionState: QuestionState.START;
   score = 0;
   questions: Map<string | number, Question> = new Map();
 
-  getCurrentQuestion =  async () => {
+  getCurrentQuestion = async () => {
     const qnId = await AjaxService.fetchCurrentQuestion();
     if (qnId) {
       return qnId;
     } else {
       return null;
     }
-  }
+  };
 
   preloadQuestion = async (qnId: number | string = 1) => {
     const question = await AjaxService.fetchQuestion(qnId);
     if (question) {
       this.questions.set(qnId, question);
-      console.log(this.questions)
+      console.log(this.questions);
     }
   };
 
-  handleEvent = (msg) => {
-    
-  }
+  handleEvent = (msg: GameState) => {
+    switch (msg.progress) {
+      case ProgressState.EMPTY:
+        //
+        this.update();
+        break;
+      case ProgressState.WAITING:
+        //
+        this.update();
+        break;
+      case ProgressState.PLAYING:
+        //
+        this.update();
+        break;
+      case ProgressState.END:
+        //
+        this.update();
+        break;
+    }
+  };
 }
